ed2169f26dfeba75118b4b4c257f096532bffece
j-watanabe <j-watanabe@ruby-dev.jp>
2023-09-07 06:47:16 +0900

    Finish layout and routes

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -9,5 +9,8 @@ class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
     assert_equal "#{@base_title}", full_title
     assert_equal "Help | #{@base_title}", full_title("Help")
+    assert_equal "About | #{@base_title}", full_title("About")
+    assert_equal "Contact | #{@base_title}", full_title("Contact")
+    assert_equal "Sign up | #{@base_title}", full_title("Sign up")
   end
 end
